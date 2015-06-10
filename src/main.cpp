f23e7cabd3ce2ac5ed3addfd2db3955a92684102
Daniel Fagnan <dnfagnan@gmail.com>
2015-06-10 16:40:38 -0600

    C++11 compatible

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include "mbed.h"
 #include "drivers/accelerometer.h"
 
-DigitalOut myled(LED1);
-/* AnalogIn x(A0); */
-/* AnalogIn y(A1); */
-/* AnalogIn z(A2); */
+DigitalOut master_led(LED1);
 Serial pc(USBTX, USBRX);
 Accelerometer acc(A0, A1, A2);
 
 int main() {
-
-    while(1) {
-
-      printf("x: (%d) \n\r", acc.gforce().x);
-      wait(0.2f);
-    }
+  while(1) {
+    printf("x: (%d) \n\r", acc.gforce().x);
+    wait(0.2f);
+  }
 }
